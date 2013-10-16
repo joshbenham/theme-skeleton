6fe1fb0f068c3f0b3d19ff0cb4164cd76c93ab32
Josh Benham <joshbenham@gmail.com>
2013-10-17 10:32:16 +1100

    Base files

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+(function () {
+  'use strict';
+
+  module.exports = function(grunt) {
+
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      scripts: [
+        'foundation/js/vendor/jquery.js',
+        'foundation/js/foundation/foundation.js',
+        'foundation/js/foundation/foundation.section.js',
+        'foundation/js/foundation/foundation.tooltips.js',
+        'js/vendor/bullseye/bullseye.js',
+        'js/vendor/picturefill/picturefill.js',
+        'js/main.js'
+      ],
+      images: [
+        'images/**/*.png',
+        'images/**/*.jpg'
+      ],
+      scss: [
+        'style.scss',
+        'foundation/scss/**/*.scss',
+        'scss/**/*.scss'
+      ],
+
+
+      /* CSS COMPONENT ---------------------------------------------- */
+
+
+      compass: {
+        dist: {
+          options: {
+            httpPath: '/',
+            cssDir: '',
+            sassDir: '',
+            imagesDir: 'images',
+            javascriptsDir: 'js',
+            specify: 'style.scss',
+            outputStyle: 'compress',
+            trace: true
+          }
+        }
+      },
+
+
+      /* JS COMPONENT ----------------------------------------------- */
+
+
+      jshint: {
+        all: ['Gruntfile.js', 'js/main.js'],
+        options: {
+          boss: true,
+          browser: true,
+          curly: true,
+          eqeqeq: true,
+          eqnull: true,
+          immed: true,
+          jquery: true,
+          latedef: true,
+          newcap: true,
+          noarg: true,
+          quotmark: true,
+          sub: true,
+          undef: true,
+          globals: {
+            module: true
+          }
+        }
+      },
+      concat: {
+        dist: {
+          src: '<%= scripts %>',
+          dest: 'js/main.min.js'
+        }
+      },
+      uglify: {
+        options:{
+          mangle: true
+        },
+        js: {
+          src: 'js/main.min.js',
+          dest:'js/main.min.js'
+        }
+      },
+
+
+      /* WATCH COMPONENT -------------------------------------------- */
+
+
+      watch: {
+        scripts: {
+          files: '<%= scripts %>',
+          tasks: ['jshint', 'concat', 'uglify']
+        },
+        css: {
+          files: '<%= scss %>',
+          tasks: ['compass']
+        },
+      }
+    });
+
+    // Load plugins.
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'compass']);
+
+  };
+
+}());
---
+++ README.md
@@ -0,0 +1,60 @@
+wordpress-theme-skeleton
+========================
+
+[Josh Benham](http://joshbenham.net)'s skeleton theme for wordpress
+
+Overview
+--------
+
+A basic [Wordpress](http://wordpress.org) skeleton theme that is responsive out of the box by
+harnessing [Zurb Foundation](http://foundation.zurb.com/)
+
+Installation
+------------
+
+Install wordpress theme
+
+```sh
+$ git clone https://github.com/joshbenham/theme-skeleton.git static-website
+$ cd static-website
+```
+
+Install SASS and Compass
+
+```sh
+$ gem update --system
+$ gem install sass
+$ gem install compass
+```
+
+Install Grunt and Plugins (might need to use sudo)
+
+```sh
+$ npm install
+```
+
+Install the standalone Foundation
+
+```sh
+$ wget https://github.com/zurb/foundation/archive/scss-standalone.zip
+$ unzip scss-standalone
+$ mv foundation-scss-standalone foundation
+$ rm scss-standalone.zip
+```
+
+Generating concatenated and combined CSS and Javascript files
+
+```sh
+$ grunt watch
+```
+
+Dependencies
+------------
+
+ - [Foundation](http://foundation.zurb.com/)
+ - [Normalize](http://necolas.github.io/normalize.css/)
+ - [Modernizr](http://modernizr.com/)
+ - [jQuery](http://jquery.com/)
+ - [Respond.js](https://github.com/scottjehl/Respond) 1.1.0
+ - [Picturefill](https://github.com/scottjehl/picturefill) 1.2.0
+ - [Waypoints](https://github.com/imakewebthings/jquery-waypoints) 2.0.3
